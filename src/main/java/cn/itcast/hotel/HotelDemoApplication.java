75a2f0042ff676a349f831342d41a8fce0ec7bfd
杨菲菲 <2959890628@qq.com>
2026-09-09 22:38:39 +0800

    Add basic auth credentials and update Elasticsearch host

--- src/main/java/cn/itcast/hotel/HotelDemoApplication.java
+++ src/main/java/cn/itcast/hotel/HotelDemoApplication.java
@@ -1,7 +1,12 @@
 package cn.itcast.hotel;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -18,8 +23,20 @@ public class HotelDemoApplication {
 
     @Bean
     public RestHighLevelClient restHighLevelClient(){
-        return new RestHighLevelClient(RestClient.builder(
-                HttpHost.create("http://192.168.150.101:9200")
-        ));
+        RestClientBuilder builder = RestClient.builder(
+                HttpHost.create("http://192.168.147.130:9200")
+        );
+        // 配置基本身份验证（用户名/密码）
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(
+                AuthScope.ANY,
+                new UsernamePasswordCredentials("elastic", "MyStrongPassword123!")
+        );
+
+        builder.setHttpClientConfigCallback(httpClientBuilder ->
+                httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
+        );
+
+        return new RestHighLevelClient(builder);
     }
 }
